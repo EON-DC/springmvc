6863bb461509f0d2194727039ee3a259c3841708
EON-DC <mun0392@gmail.com>
2022-10-16 18:36:53 +0900

    Spring MVC 기본구조

---
+++ src/main/java/hello/springmvc/basic/response/ResponseViewController.java
@@ -0,0 +1,34 @@
+package hello.springmvc.basic.response;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ResponseViewController {
+
+    @RequestMapping("/response-view-v1")
+    public ModelAndView responseViewV1() {
+        ModelAndView mav = new ModelAndView("response/hello")
+                .addObject("data", "hello!");
+        return mav;
+    }
+
+    @RequestMapping("/response-view-v2")
+    public String responseVeiwV2(Model model) {
+        model.addAttribute("data", "hello!");
+        return "response/hello";
+    }
+
+    /**
+     * HTTP 메시지 바디를 처리하는 파라미터가 없으면 요청 URL을 참고해서 논리 뷰 이름으로 사용함
+     * 이렇게 void 를 반환할 경우 명시성이 떨어지고, 딱 맞는 경우가 많지 않으므로 권장하지 않는 방법
+     * 요청 URL: /response/hello
+     * 실행 뷰 : template/response/hello.html
+     */
+    @RequestMapping("/response/hello")
+    public void responseViewV3(Model model) {
+        model.addAttribute("data", "hello!");
+    }
+}
